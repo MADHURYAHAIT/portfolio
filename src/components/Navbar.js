205c0ddc8228ec3699925bebc98aefc2d88e3e08
Madhurya Hait <madhurya_202100516@smit.smu.edu.in>
2023-12-14 00:37:07 +0530

    border

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,37 +20,7 @@ export default function Navbar() {
       {/* <nav className={navbar ? "navbar active navbar-expand-lg navbar-dark fixed-top" : "navbar navbar-expand-lg navbar-dark fixed-top"}> */}
 
 
-  {/* <div className="container-fluid">
-       <a class="navbar-brand" href="#">P.</a>
-    <a className="navbar-brand" href="https://github.com/pooranjoyb/portfolio-website"><img src="./img/web.png" width='30' alt=""/></a>
-    
-    
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"  aria-label="Close">
-      <span className="navbar-toggler-icon"></span> 
-    </button>
-
   
-    
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto" id='navbar-list'>
-        <li className="nav-item px-2">
-          <a className="nav-link" aria-current="page" href="">Home</a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link" href="#intro">About</a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link" href="#projects">Projects</a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link" href="https://drive.google.com/file/d/1p3-WJZ-VViv5MDXe5CfMAbPaYaxFuBpK/view?usp=sharing" target="_blank">Resume</a>
-        </li>
-        <li className="nav-item px-2">
-          <a className="nav-link" href="#contact">Contact</a>
-        </li>
-      </ul>      
-    </div>
-  </div> */}
 
     <nav className={navbarhai? "navbar navbar-dark anim fixed-top" : "navoff"} >
   <div class="container-fluid" >
