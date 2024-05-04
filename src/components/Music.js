53151899b1957661e40b83e5591ac7f3f66fb08b
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2024-05-04 22:52:45 +0530

    .

--- src/components/Music.js
+++ src/components/Music.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
-import Carousel from 'react-elastic-carousel';
+
 
 const breakpoints = [
     { width: 1, itemsToShow: 1 },
